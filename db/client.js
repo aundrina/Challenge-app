ab9c3080bde13a54cf0e5d4523a6a8ba02ef2355
Aundrina <aundrinaleyva@yahoo.com>
2023-01-17 16:36:16 -0800

    initial commit

--- db/client.js
+++ db/client.js
@@ -1,1 +1,9 @@
-// hello world
+const { Client } = require("pg");
+
+const dbName = "Challenge-App";
+
+const client = new Client({
+  connectionString:
+    process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`,
+});
+module.exports = client;
